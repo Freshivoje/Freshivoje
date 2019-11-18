43fb6491fc25a8fbc215d4e4413f1437a63d7da8
Mladen Dubovac <mladendubovac@live.com>
2019-11-18 14:57:10 +0100

    Remove excess imports.

--- Freshivoje/Transport/TransportForm.cs
+++ Freshivoje/Transport/TransportForm.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Freshivoje.Custom_Forms;
 using Freshivoje.Models;
-using MySql.Data.MySqlClient;
 
 namespace Freshivoje.Transport
 {
