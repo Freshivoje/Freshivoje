648973035873194a03e941afae6af2bac38a767f
aca sax <acasax@gmail.com>
2019-11-19 12:54:36 +0100

    SAMO SAM POCEO pa sam ispizdeo

---
+++ Freshivoje/Storage/StorageForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Freshivoje.Storage
+{
+    partial class StorageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StorageForm));
+            this.StorageFormTblLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.recivedArticlesLbl = new System.Windows.Forms.Label();
+            this.tunnelLbl = new System.Windows.Forms.Label();
+            this.chambersLbl = new System.Windows.Forms.Label();
+            this.finishInsertBtn = new System.Windows.Forms.Button();
+            this.StorageFormTblLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StorageFormTblLayout
+            // 
+            this.StorageFormTblLayout.BackColor = System.Drawing.Color.Transparent;
+            this.StorageFormTblLayout.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("StorageFormTblLayout.BackgroundImage")));
+            this.StorageFormTblLayout.ColumnCount = 10;
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.11441F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.56931F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.18977F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.12651F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.StorageFormTblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.StorageFormTblLayout.Controls.Add(this.tunnelLbl, 1, 2);
+            this.StorageFormTblLayout.Controls.Add(this.recivedArticlesLbl, 1, 1);
+            this.StorageFormTblLayout.Controls.Add(this.exitBtn, 9, 0);
+            this.StorageFormTblLayout.Controls.Add(this.lblTitle, 0, 0);
+            this.StorageFormTblLayout.Controls.Add(this.backBtn, 8, 0);
+            this.StorageFormTblLayout.Controls.Add(this.chambersLbl, 3, 1);
+            this.StorageFormTblLayout.Controls.Add(this.finishInsertBtn, 3, 2);
+            this.StorageFormTblLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StorageFormTblLayout.Location = new System.Drawing.Point(0, 0);
+            this.StorageFormTblLayout.Name = "StorageFormTblLayout";
+            this.StorageFormTblLayout.RowCount = 15;
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666668F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666668F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.StorageFormTblLayout.Size = new System.Drawing.Size(2034, 864);
+            this.StorageFormTblLayout.TabIndex = 3;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.BackgroundImage = global::Freshivoje.Properties.Resources.x;
+            this.exitBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.exitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exitBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.ForeColor = System.Drawing.Color.White;
+            this.exitBtn.Location = new System.Drawing.Point(1985, 3);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
+            this.exitBtn.Size = new System.Drawing.Size(46, 51);
+            this.exitBtn.TabIndex = 6;
+            this.exitBtn.TabStop = false;
+            this.exitBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.exitBtn.UseVisualStyleBackColor = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.StorageFormTblLayout.SetColumnSpan(this.lblTitle, 3);
+            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(3, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.lblTitle.Size = new System.Drawing.Size(147, 57);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "MAGACIN";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // backBtn
+            // 
+            this.backBtn.BackColor = System.Drawing.Color.Transparent;
+            this.backBtn.BackgroundImage = global::Freshivoje.Properties.Resources.left_arrow;
+            this.backBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.backBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.backBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.backBtn.FlatAppearance.BorderSize = 0;
+            this.backBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.backBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.backBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backBtn.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backBtn.ForeColor = System.Drawing.Color.White;
+            this.backBtn.Location = new System.Drawing.Point(1935, 3);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(44, 51);
+            this.backBtn.TabIndex = 9;
+            this.backBtn.TabStop = false;
+            this.backBtn.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.backBtn.UseVisualStyleBackColor = false;
+            // 
+            // recivedArticlesLbl
+            // 
+            this.recivedArticlesLbl.AutoSize = true;
+            this.recivedArticlesLbl.BackColor = System.Drawing.Color.Transparent;
+            this.recivedArticlesLbl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.recivedArticlesLbl.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recivedArticlesLbl.ForeColor = System.Drawing.Color.White;
+            this.recivedArticlesLbl.Location = new System.Drawing.Point(153, 87);
+            this.recivedArticlesLbl.Name = "recivedArticlesLbl";
+            this.recivedArticlesLbl.Size = new System.Drawing.Size(323, 27);
+            this.recivedArticlesLbl.TabIndex = 42;
+            this.recivedArticlesLbl.Text = "Primljeni artikli :";
+            this.recivedArticlesLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tunnelLbl
+            // 
+            this.tunnelLbl.AutoSize = true;
+            this.tunnelLbl.BackColor = System.Drawing.Color.Transparent;
+            this.tunnelLbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tunnelLbl.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tunnelLbl.ForeColor = System.Drawing.Color.White;
+            this.tunnelLbl.Location = new System.Drawing.Point(153, 114);
+            this.tunnelLbl.Name = "tunnelLbl";
+            this.StorageFormTblLayout.SetRowSpan(this.tunnelLbl, 12);
+            this.tunnelLbl.Size = new System.Drawing.Size(323, 684);
+            this.tunnelLbl.TabIndex = 43;
+            this.tunnelLbl.Text = "1.";
+            this.tunnelLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chambersLbl
+            // 
+            this.chambersLbl.AutoSize = true;
+            this.chambersLbl.BackColor = System.Drawing.Color.Transparent;
+            this.StorageFormTblLayout.SetColumnSpan(this.chambersLbl, 3);
+            this.chambersLbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chambersLbl.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chambersLbl.ForeColor = System.Drawing.Color.White;
+            this.chambersLbl.Location = new System.Drawing.Point(502, 57);
+            this.chambersLbl.Name = "chambersLbl";
+            this.chambersLbl.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.chambersLbl.Size = new System.Drawing.Size(1061, 57);
+            this.chambersLbl.TabIndex = 44;
+            this.chambersLbl.Text = "KOMORE ";
+            this.chambersLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // finishInsertBtn
+            // 
+            this.finishInsertBtn.BackColor = System.Drawing.Color.Transparent;
+            this.finishInsertBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.finishInsertBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.finishInsertBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.finishInsertBtn.FlatAppearance.BorderSize = 5;
+            this.finishInsertBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.finishInsertBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.finishInsertBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.finishInsertBtn.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finishInsertBtn.ForeColor = System.Drawing.Color.White;
+            this.finishInsertBtn.Location = new System.Drawing.Point(504, 119);
+            this.finishInsertBtn.Margin = new System.Windows.Forms.Padding(5);
+            this.finishInsertBtn.Name = "finishInsertBtn";
+            this.StorageFormTblLayout.SetRowSpan(this.finishInsertBtn, 5);
+            this.finishInsertBtn.Size = new System.Drawing.Size(517, 275);
+            this.finishInsertBtn.TabIndex = 45;
+            this.finishInsertBtn.TabStop = false;
+            this.finishInsertBtn.Text = "A1";
+            this.finishInsertBtn.UseVisualStyleBackColor = false;
+            // 
+            // StorageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2034, 864);
+            this.Controls.Add(this.StorageFormTblLayout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StorageForm";
+            this.Text = "StorageForm";
+            this.StorageFormTblLayout.ResumeLayout(false);
+            this.StorageFormTblLayout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel StorageFormTblLayout;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Label tunnelLbl;
+        private System.Windows.Forms.Label recivedArticlesLbl;
+        private System.Windows.Forms.Label chambersLbl;
+        private System.Windows.Forms.Button finishInsertBtn;
+    }
+}
