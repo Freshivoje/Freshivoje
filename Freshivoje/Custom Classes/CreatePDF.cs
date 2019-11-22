ab8d1899b16a24420ee7474fa9b4da7a83bbdf47
aca sax <acasax@gmail.com>
2019-11-22 16:29:12 +0100

    Revert "Update CreatePDF class."
    This reverts commit d39bc1a5df8b1df4cad155f70f28188b50201517.

--- Freshivoje/Custom Classes/CreatePDF.cs
+++ Freshivoje/Custom Classes/CreatePDF.cs
@@ -5,18 +5,14 @@ using System.IO;
 using System.Windows.Forms;
 using Freshivoje.Models;
 
-namespace Freshivoje.Custom_Classes
+namespace Freshivoje.Custom_Forms
 {
-    public class CreatePDF
+    public class PDF
     {
         private int _fkClientId;
         private string _clientFirstName, _clientLastName, _clientAddress, _clientJMBG, _clientBPG, _clientZipCode, _clientPhone;
-        
-        private Font _font;
-        public CreatePDF(Client client)
+        public PDF(Client client)
         {
-            _font = GetTahoma();
-         
             _fkClientId = client._id;
             _clientFirstName = client._firstName;
             _clientLastName = client._lastName;
@@ -27,20 +23,10 @@ namespace Freshivoje.Custom_Classes
             _clientPhone = client._phone;
         }
 
-        public static Font GetTahoma()
-        {
-            var fontName = "Tahoma";
-            if (!FontFactory.IsRegistered(fontName))
-            {
-                var fontPath = Environment.GetEnvironmentVariable("SystemRoot") + "\\fonts\\tahoma.ttf";
-                FontFactory.Register(fontPath);
-            }
-            return FontFactory.GetFont(fontName, BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 12);
-        }
+
         public void OnEndPage(PdfWriter writer)
         {
-            Paragraph footer = new Paragraph("________________                                                                          ________________", _font);
-            footer.Font.Size = 12;
+            Paragraph footer = new Paragraph("________________                                                                                               ________________", FontFactory.GetFont(FontFactory.TIMES, 12, iTextSharp.text.Font.NORMAL));
             footer.Alignment = Element.ALIGN_CENTER;
             PdfPTable footerTbl = new PdfPTable(1);
             footerTbl.TotalWidth = 1000;
@@ -53,8 +39,7 @@ namespace Freshivoje.Custom_Classes
         }
         public void OnEndPage1(PdfWriter writer)
         {
-            Paragraph footer2 = new Paragraph("Potpis klijenta:                                                                                 Potpis izdavaoca:", _font);
-            footer2.Font.Size = 12;
+            Paragraph footer2 = new Paragraph("Potpis klijenta:                                                                                                       Potpis izdavaoca:", FontFactory.GetFont(FontFactory.TIMES, 12, iTextSharp.text.Font.NORMAL));
             footer2.Alignment = Element.ALIGN_CENTER;
             PdfPTable footerTb2 = new PdfPTable(1);
             footerTb2.TotalWidth = 1000;
@@ -113,12 +98,10 @@ namespace Freshivoje.Custom_Classes
                 total.DefaultCell.HorizontalAlignment = Element.ALIGN_RIGHT;
                 total.DefaultCell.VerticalAlignment = Element.ALIGN_RIGHT;
 
-                
-                data.AddCell(new Phrase("JEDINIČNA CENA \n(BR.)", _font));
-                data.AddCell(new Phrase("KOLIČINA \n(KG)", _font));
-                data.AddCell(new Phrase("KILOMETRI \n(KM)", _font));
-                data.AddCell(new Phrase("UKUPNA CENA \n(RSD)", _font));
-              
+                data.AddCell(new Phrase("JEDINIČNA CENA \n(BR.)"));
+                data.AddCell(new Phrase("KOLIČINA \n(KG)"));
+                data.AddCell(new Phrase("KILOMETRI \n(KM)"));
+                data.AddCell(new Phrase("UKUPNA CENA \n(RSD)"));
 
                 foreach (DataGridViewRow row in dgw.Rows)
                 {
@@ -134,122 +117,122 @@ namespace Freshivoje.Custom_Classes
                     finalPrice = finalPrice + totalPrice1;
                 }
 
-                Chunk c1 = new Chunk("REŠIVOJE", _font);
+                Chunk c1 = new Chunk("REŠIVOJE", FontFactory.GetFont("Times New Roman"));
                 c1.Font.Color = new BaseColor(0, 0, 0);
                 c1.Font.SetStyle(0);
-                c1.Font.Size = 12;
+                c1.Font.Size = 16;
                 Phrase p1 = new Phrase();
                 p1.Add(c1);
                 companyName.AddCell(p1);
-                Chunk c3 = new Chunk("Aleksandar Đorđević", _font);
+                Chunk c3 = new Chunk("Aleksandar Đorđevic", FontFactory.GetFont("Times New Roman"));
                 c3.Font.Color = new BaseColor(0, 0, 0);
                 c3.Font.SetStyle(0);
                 c3.Font.Size = 11;
                 Phrase p3 = new Phrase();
                 p3.Add(c3);
                 companyData.AddCell(p3);
-                Chunk c4 = new Chunk("PIB: 1118657152", _font);
-                c3.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c4 = new Chunk("PIB: 1118657152", FontFactory.GetFont("Times New Roman"));
+                c3.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c3.Font.SetStyle(0);
                 c3.Font.Size = 11;
                 Phrase p4 = new Phrase();
                 p4.Add(c4);
                 companyData.AddCell(p4);
-                Chunk c5 = new Chunk("Matični broj: 65511037", _font);
-                c3.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c5 = new Chunk("Maticni broj: 65511037", FontFactory.GetFont("Times New Roman"));
+                c3.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c3.Font.SetStyle(0);
                 c3.Font.Size = 11;
                 Phrase p5 = new Phrase();
                 p5.Add(c5);
                 companyData.AddCell(p5);
-                Chunk c6 = new Chunk("Tekući račun: 205-0000000270172-07", _font);
-                c3.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c6 = new Chunk("Tekuci racun: 205-0000000270172-07", FontFactory.GetFont("Times New Roman"));
+                c3.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c3.Font.SetStyle(0);
                 c3.Font.Size = 11;
                 Phrase p6 = new Phrase();
                 p6.Add(c6);
                 companyData.AddCell(p6);
-                Chunk c7 = new Chunk("Adresa: Jasički put 9, 37000, Kruševac", _font);
-                c3.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c7 = new Chunk("Adresa: Jasicki put 9, 37000, Krusevac", FontFactory.GetFont("Times New Roman"));
+                c3.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c3.Font.SetStyle(0);
                 c3.Font.Size = 11;
                 Phrase p7 = new Phrase();
                 p7.Add(c7);
                 companyData.AddCell(p7);
                 //////////////////////////////////////////////////////////////////////////////////////////////////
-                Chunk c8 = new Chunk(("Ime: ") + _clientFirstName, _font);
-                c8.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c8 = new Chunk(("Ime: ") + _clientFirstName, FontFactory.GetFont("Times New Roman"));
+                c8.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c8.Font.SetStyle(0);
                 c8.Font.Size = 11;
                 Phrase p8 = new Phrase();
                 p8.Add(c8);
                 clientData.AddCell(p8);
-                Chunk c9 = new Chunk(("Prezime: ") + _clientLastName, _font);
-                c9.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c9 = new Chunk(("Prezime: ") + _clientLastName, FontFactory.GetFont("Times New Roman"));
+                c9.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c9.Font.SetStyle(0);
                 c9.Font.Size = 11;
                 Phrase p9 = new Phrase();
                 p9.Add(c9);
                 clientData.AddCell(p9);
-                Chunk c10 = new Chunk(("Adresa: ") + _clientAddress, _font);
-                c10.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c10 = new Chunk(("Adresa: ") + _clientAddress, FontFactory.GetFont("Times New Roman"));
+                c10.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c10.Font.SetStyle(0);
                 c10.Font.Size = 11;
                 Phrase p10 = new Phrase();
                 p10.Add(c10);
                 clientData.AddCell(p10);
-                Chunk c11 = new Chunk(("JMBG: ") + _clientJMBG, _font);
-                c11.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c11 = new Chunk(("JMBG: ") + _clientJMBG, FontFactory.GetFont("Times New Roman"));
+                c11.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c11.Font.SetStyle(0);
                 c11.Font.Size = 11;
                 Phrase p11 = new Phrase();
                 p11.Add(c11);
                 clientData.AddCell(p11);
-                Chunk c12 = new Chunk(("BPG: ") + _clientBPG, _font);
-                c12.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c12 = new Chunk(("BPG: ") + _clientBPG, FontFactory.GetFont("Times New Roman"));
+                c12.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c12.Font.SetStyle(0);
                 c12.Font.Size = 11;
                 Phrase p12 = new Phrase();
                 p12.Add(c12);
                 clientData.AddCell(p12);
-                Chunk c13 = new Chunk(("Poštanski broj: ") + _clientZipCode, _font);
-                c13.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c13 = new Chunk(("Postanski broj: ") + _clientZipCode, FontFactory.GetFont("Times New Roman"));
+                c13.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c13.Font.SetStyle(0);
                 c13.Font.Size = 11;
                 Phrase p13 = new Phrase();
                 p13.Add(c13);
                 clientData.AddCell(p13);
-                Chunk c14 = new Chunk(("Telefon: ") + _clientPhone, _font);
-                c14.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c14 = new Chunk(("Telefon: ") + _clientPhone, FontFactory.GetFont("Times New Roman"));
+                c14.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c14.Font.SetStyle(0);
                 c14.Font.Size = 11;
                 Phrase p14 = new Phrase();
                 p14.Add(c14);
                 clientData.AddCell(p14);
                 //////////////////////////////////////////////////////////////////////////////////////////////////
-                Chunk c15 = new Chunk(" ", _font);
-                c15.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c15 = new Chunk(" ", FontFactory.GetFont("Times New Roman"));
+                c15.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c15.Font.SetStyle(0);
                 c15.Font.Size = 20;
                 Phrase p15 = new Phrase();
                 p15.Add(c15);
                 total.AddCell(p15);
-                Chunk c16 = new Chunk(" ", _font);
-                c16.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c16 = new Chunk(" ", FontFactory.GetFont("Times New Roman"));
+                c16.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c16.Font.SetStyle(0);
                 c16.Font.Size = 18;
                 Phrase p16 = new Phrase();
                 p16.Add(c16);
                 total.AddCell(p16);
-                Chunk c17 = new Chunk("UKUPNO: ", _font);
-                c17.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c17 = new Chunk("UKUPNO: ", FontFactory.GetFont("Times New Roman"));
+                c17.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c17.Font.SetStyle(0);
                 c17.Font.Size = 18;
                 Phrase p17 = new Phrase();
                 p17.Add(c17);
                 total.AddCell(p17);
-                Chunk c18 = new Chunk(finalPrice.ToString() + " RSD", _font);
-                c18.Font.Color = new BaseColor(0, 0, 0);
+                Chunk c18 = new Chunk(finalPrice.ToString() + " RSD", FontFactory.GetFont("Times New Roman"));
+                c18.Font.Color = new iTextSharp.text.BaseColor(0, 0, 0);
                 c18.Font.SetStyle(0);
                 c18.Font.Size = 18;
                 Phrase p18 = new Phrase();
@@ -257,21 +240,21 @@ namespace Freshivoje.Custom_Classes
                 total.AddCell(p18);
                 #endregion
                 #region IMAGE SECTION
-               // pictureURL = "C:\\Users\\KORISNIK\\source\\repos\\Freshivoje\\Freshivoje\\Freshivoje\\Images\\1.png";
-                //iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(pictureURL);
-                //logo.ScaleToFit(110.0f, 140.0f);
-                //logo.SpacingBefore = 20.0f;
-                //logo.SpacingAfter = 10.0f;
-                //logo.SetAbsolutePosition(70, 680);
-                //logo.Alignment = Element.ALIGN_CENTER;
+                pictureURL = "C:\\Users\\KORISNIK\\source\\repos\\Freshivoje\\Freshivoje\\Freshivoje\\Images\\1.png";
+                iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(pictureURL);
+                logo.ScaleToFit(110.0f, 140.0f);
+                logo.SpacingBefore = 20.0f;
+                logo.SpacingAfter = 10.0f;
+                logo.SetAbsolutePosition(70, 680);
+                logo.Alignment = Element.ALIGN_CENTER;
                 #endregion
                 #region PDF
-                folderPath = "C:\\PDF\\";
+                folderPath = "D:\\PDF\\";
                 if (!Directory.Exists(folderPath))
                 {
                     Directory.CreateDirectory(folderPath);
                 }
-                fileCount = Directory.GetFiles("C:\\PDF").Length;
+                fileCount = Directory.GetFiles("D:\\PDF").Length;
                 strFileName = (fileCount + 1) + "-2019" + ".pdf";
                 invoiceNumber.AddCell("BROJ FAKTURE: TR" + ((fileCount + 1) + "-2019"));
                 #endregion
@@ -281,7 +264,7 @@ namespace Freshivoje.Custom_Classes
                     Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 70f);
                     PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
                     pdfDoc.Open();
-                  //  pdfDoc.Add(logo);
+                    pdfDoc.Add(logo);
                     pdfDoc.Add(blank);
                     pdfDoc.Add(companyName);
                     pdfDoc.Add(blank);
