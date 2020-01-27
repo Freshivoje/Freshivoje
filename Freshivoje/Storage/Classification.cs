22e0ec5f9661adf12cbcd801cd12f6bbe3e1da03
aca sax <acasax@gmail.com>
2020-01-27 15:59:15 +0100

    classification

--- Freshivoje/Storage/Classification.cs
+++ Freshivoje/Storage/Classification.cs
@@ -16,7 +16,7 @@ namespace Freshivoje.Storage
         readonly string query = "SELECT receipts.fk_client_id, clients.first_name, clients.last_name, items_receipt.fk_receipt_id, items_receipt.fk_article_id, items_receipt.id_items_receipt, items_receipt.quantity, items_receipt.status, articles.article_name, articles.sort, articles.organic, articles.category, item_pallete.fk_id_pallete, item_pallete.fk_id_item_recepit, pallete.pallet_number  FROM receipts INNER JOIN clients ON receipts.fk_client_id = clients.id_client INNER JOIN items_receipt ON items_receipt.fk_receipt_id = receipts.id_receipt INNER JOIN articles ON articles.id_article = items_receipt.fk_article_id INNER JOIN item_pallete ON items_receipt.id_items_receipt=item_pallete.fk_id_item_recepit INNER JOIN pallete on pallete.id_pallete = item_pallete.fk_id_pallete";
         public Classification()
         {
-            DbConnection.FillCmbBoxQuery(palletnumberCmbBox, query1, "id_pallete", "pallet_number", "classification");
+            DbConnection.fillCmbBox(palletnumberCmbBox, "pallete", '/', "id_pallete", "pallet_number");
             InitializeComponent();
         }
 
